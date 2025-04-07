9e27b9d66735e0462aa2f2717d7bb293a04653f6
Lê Tí <106995604+arm64x@users.noreply.github.com>
2025-04-08 00:19:40 +0700

    Update LibraryViewController+Import.swift

--- iOS/Views/Apps/LibraryViewController+Import.swift
+++ iOS/Views/Apps/LibraryViewController+Import.swift
@@ -212,57 +212,59 @@ extension LibraryViewController {
 
 
 extension LibraryViewController {
-	@objc func startInstallProcess(meow: NSManagedObject, filePath: String) {
-		UIApplication.shared.isIdleTimerDisabled = true
-		
-		let name = (meow.value(forKey: "name") as? String) ?? "UnknownApp"
-		let id = (meow.value(forKey: "bundleidentifier") as? String) ?? "stupidfucking.shit"
-		let version = (meow.value(forKey: "version") as? String) ?? "1.0"
-		
-		self.presentTransferPreview(with: filePath, id: id, version: version, name: name)
-	}
-	
-	@objc func shareFile(meow: NSManagedObject, filePath: String) {
-		UIApplication.shared.isIdleTimerDisabled = true
-		
-		let name = (meow.value(forKey: "name") as? String) ?? "UnknownApp"
-		let id = (meow.value(forKey: "bundleidentifier") as? String) ?? "stupidfucking.shit"
-		let version = (meow.value(forKey: "version") as? String) ?? "1.0"
-		
-		self.presentTransferPreview(with: filePath, isSharing: true, id: id, version: version, name: name)
-	}
-	
-	func presentTransferPreview(with appPath: String, isSharing: Bool? = false, id: String, version: String, name: String) {
-		do {
-			self.installer = try Installer(
-				path: nil,
-				metadata: AppData(id: id, version: Int(version) ?? Int(1.0), name: name)
-			)
-			
-			let transferPreview = TransferPreview(installer: self.installer!, appPath: appPath, appName: name, isSharing: isSharing ?? false)
-				.onDisappear {
-					self.installer?.shutdownServer()
-					self.installer = nil
-					UIApplication.shared.isIdleTimerDisabled = true
-				}
-			
-			let hostingController = UIHostingController(rootView: transferPreview)
-			hostingController.modalPresentationStyle = .pageSheet
-			
-			if let presentationController = hostingController.presentationController as? UISheetPresentationController {
-				let detent2: UISheetPresentationController.Detent = ._detent(withIdentifier: "Test2", constant: 200.0)
-				presentationController.detents = [detent2]
-				presentationController.prefersGrabberVisible = true
-			}
-			
-			DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-				self.present(hostingController, animated: true)
-			}
-			
-		} catch {
-			self.installer?.shutdownServer()
-			self.installer = nil
-		}
-	}
-	
+    @objc func startInstallProcess(meow: NSManagedObject, filePath: String) {
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        let name = (meow.value(forKey: "name") as? String) ?? "UnknownApp"
+        let id = (meow.value(forKey: "bundleidentifier") as? String) ?? "stupidfucking.shit"
+        let version = (meow.value(forKey: "version") as? String) ?? "1.0"
+        
+        self.presentTransferPreview(with: filePath, id: id, version: version, name: name)
+    }
+    
+    @objc func shareFile(meow: NSManagedObject, filePath: String) {
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        let name = (meow.value(forKey: "name") as? String) ?? "UnknownApp"
+        let id = (meow.value(forKey: "bundleidentifier") as? String) ?? "stupidfucking.shit"
+        let version = (meow.value(forKey: "version") as? String) ?? "1.0"
+        
+        self.presentTransferPreview(with: filePath, isSharing: true, id: id, version: version, name: name)
+    }
+    
+    func presentTransferPreview(with appPath: String, isSharing: Bool? = false, id: String, version: String, name: String) {
+        do {
+            // Chuyển đổi chuỗi version thành Float thay vì Int
+            let versionFloat = Float(version) ?? 1.0
+            
+            self.installer = try Installer(
+                path: nil,
+                metadata: AppData(id: id, version: versionFloat, name: name)
+            )
+            
+            let transferPreview = TransferPreview(installer: self.installer!, appPath: appPath, appName: name, isSharing: isSharing ?? false)
+                .onDisappear {
+                    self.installer?.shutdownServer()
+                    self.installer = nil
+                    UIApplication.shared.isIdleTimerDisabled = true
+                }
+            
+            let hostingController = UIHostingController(rootView: transferPreview)
+            hostingController.modalPresentationStyle = .pageSheet
+            
+            if let presentationController = hostingController.presentationController as? UISheetPresentationController {
+                let detent2: UISheetPresentationController.Detent = ._detent(withIdentifier: "Test2", constant: 200.0)
+                presentationController.detents = [detent2]
+                presentationController.prefersGrabberVisible = true
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.present(hostingController, animated: true)
+            }
+            
+        } catch {
+            self.installer?.shutdownServer()
+            self.installer = nil
+        }
+    }
 }
